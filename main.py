90faefc34de26d1bc08fdfd5e3f0eb4e22fc0942
malirajko <bytesmcgregor@gmail.com>
2026-07-09 01:24:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -42,9 +42,8 @@ class StaffFormular(discord.ui.Modal, title="Prijava za Staff Tim"):
     async def on_submit(self, interaction: discord.Interaction):
         await interaction.response.defer(ephemeral=True)
         
-        # Pravljenje privatnog kanala (Tiketa) za prijavu
         guild = interaction.guild
-        admin_role = discord.utils.get(guild.roles, name="Admin") # Promeni ime role ako se zove drugačije
+        admin_role = discord.utils.get(guild.roles, name="Admin")
         
         overwrites = {
             guild.default_role: discord.PermissionOverwrite(read_messages=False),
@@ -56,7 +55,6 @@ class StaffFormular(discord.ui.Modal, title="Prijava za Staff Tim"):
         kanal_ime = f"prijava-{interaction.user.name}"
         ticket_channel = await guild.create_text_channel(name=kanal_ime, overwrites=overwrites)
 
-        # Slanje rezultata formulara u kreirani tiket
         embed = discord.Embed(title=f"Nova Staff Prijava - {interaction.user.display_name}", color=discord.Color.green())
         embed.add_field(name="Ime i Godište", value=self.ime.value, inline=False)
         embed.add_field(name="Discord", value=self.discord_username.value, inline=False)
@@ -75,64 +73,4 @@ class TicketButton(discord.ui.View):
         super().__init__(timeout=None)
 
     @discord.ui.button(label="Otvori Konkurs", style=discord.ButtonStyle.primary, custom_id="otvori_konkurs_dugme")
-    async def otvori_konkurs(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_modal(StaffFormular())
-
-class TicketZatvoriView(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
-
-    @discord.ui.button(label="Zatvori Tiket", style=discord.ButtonStyle.danger, custom_id="zatvori_tiket_dugme")
-    async def zatvori_tiket(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message("Ovaj tiket će biti obrisan za 5 sekundi...")
-        await discord.utils.sleep_until(discord.utils.utcnow() + discord.utils.datetime.timedelta(seconds=5))
-        await interaction.channel.delete()
-
-# ==========================================
-# 5. DOGAĐAJI (EVENTS)
-# ==========================================
-@bot.event
-async def on_ready():
-    print(f'Ticket bot spreman!')
-    bot.add_view(TicketButton()) # Da dugme radi i nakon restarta bota
-    bot.add_view(TicketZatvoriView())
-
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        return
-    raise error
-
-# ==========================================
-# 6. BOT KOMANDE
-# ==========================================
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def setup(ctx):
-    embed = discord.Embed(
-        title="📋 Konkurs za Staff Tim",
-        description="Ukoliko želiš da postaneš deo našeg Staff tima, klikni na dugme ispod i odgovori na pitanja u formularu.",
-        color=discord.Color.purple()
-    )
-    await ctx.send(embed=embed, view=TicketButton())
-
-@bot.command(name="help")
-async def ticket_help(ctx):
-    poruka = (
-        "<:arrow_join6:1516798345568456714> **__Next Level Ticket Help Bot__**\n\n"
-        "<:arrow_join6:1516798345568456714> Kako da se prijavis?\n"
-        "- __U kanalu: https://discord.com/channels/1404528302559068200/1523627320852873348 kliknite na plavo dugme/gumb__ **Otvori Konkurs** __nakon toga ce vam izaci formular koji morate da popunite nakon toga ce prijava biti poslata.__\n\n"
-        "<:arrow_join6:1516798345568456714> Sta treba da bi postali deo administracije?\n"
-        "- __Potrebno je osnovno znanje u SAMP modovima,kreshovima itd, aktivan facebook nalog,aktivnost na serveru,kulturnost,znanje da se resi odredjena situacija.__\n\n"
-        "<:arrow_join6:1516798345568456714> Privilegije nase administracije\n"
-        "- __Kao deo nase administracije na kraju svakog meseca clan koji se najbolje dokaze dobija neku placenu platformu kao sto su: Netflix,Spotify Premium,HBO,Disney ili nitro pretplata.__\n\n"
-        "<:arrow_join6:1516798345568456714> **UKOLIKO ZELIS DA POSTANES DEO ADMINSTRACIJE JAVI NAM SE PUTEM TIKETA !**"
-    )
-    await ctx.send(poruka)
-
-# ==========================================
-# 7. POKRETANJE BOTA
-# ==========================================
-keep_alive()
-token = os.environ.get("DISCORD_TOKEN")
-bot.run(token)
+    async def otvori_konkurs(self, interaction: discord.Interaction
